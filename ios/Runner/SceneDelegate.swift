d53d39f2f38577b5e4a64bf27ecb77ce2b631f9c
lijiacheng <1587088806@qq.com>
2026-02-27 17:01:01 +0800

    Fix iOS - remove UIMainStoryboardFile and add URL Scheme
    - Remove UIMainStoryboardFile that was causing white screen
    - Add CFBundleURLTypes for expense-tracker:// scheme
    - Keep SceneDelegate and AppDelegate simple

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -15,23 +15,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     window = UIWindow(windowScene: windowScene)
     window?.rootViewController = flutterViewController
     window?.makeKeyAndVisible()
-
-    // 处理启动时的 URL
-    if let url = connectionOptions.urlContexts.first?.url {
-      _ = flutterViewController.handleOpen(url)
-    }
   }
 
-  func sceneDidDisconnect(_ scene: UIScene) {}
-  func sceneDidBecomeActive(_ scene: UIScene) {}
-  func sceneWillResignActive(_ scene: UIScene) {}
-  func sceneWillEnterForeground(_ scene: UIScene) {}
-  func sceneDidEnterBackground(_ scene: UIScene) {}
+  func sceneDidDisconnect(_ scene: UIScene) {
+  }
 
-  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-    if let url = URLContexts.first?.url,
-       let flutterViewController = window?.rootViewController as? FlutterViewController {
-      _ = flutterViewController.handleOpen(url)
-    }
+  func sceneDidBecomeActive(_ scene: UIScene) {
+  }
+
+  func sceneWillResignActive(_ scene: UIScene) {
+  }
+
+  func sceneWillEnterForeground(_ scene: UIScene) {
+  }
+
+  func sceneDidEnterBackground(_ scene: UIScene) {
   }
 }
