3be1e8e173ee61e3f6dfa5ff42bdf655bd19e566
lijiacheng <1587088806@qq.com>
2026-02-28 14:27:31 +0800

    Fix: correctly forward url scheme opening events to Flutter in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,14 @@ class AppDelegate: FlutterAppDelegate {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  
+  // 重写该方法以手动接管自定义 URL scheme 的唤醒 (Deep Link) 
+  // 这对 uni_links 在某些 iOS 版本的后台热启动十分重要
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    return super.application(app, open: url, options: options)
+  }
 }
